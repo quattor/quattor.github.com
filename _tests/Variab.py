6fe182c8c18d2c0c89ce83a400911075c83761f8
Devdeve <russell.seers@gmail.com>
2016-04-04 11:31:33 +0100

    Update Variab.py
    Makes the filenames list direct to the right place and adding in notes

--- _tests/Variab.py
+++ _tests/Variab.py
@@ -1,13 +1,12 @@
-
 import glob, os, enchant, codecs, logging, json, os.path, shutil, re
 from enchant.checker import SpellChecker
-logger = logging.getLogger('spellcheck')
-pwl = enchant.request_pwl_dict("Dict.txt")
-chkr = SpellChecker("en_GB")
-checker = SpellChecker("en_GB")
-filenameslist = glob.glob("../_posts/*.md")
+from enchant.tokenize import EmailFilter, URLFilter
+#logger = logging.getLogger('spellcheck')
+pwl = enchant.request_pwl_dict("Dict.txt") #add words to the dictionary used to test for spelling errors
+chkr = SpellChecker("en_GB", filters=[URLFilter, EmailFilter])
+#chkr1 = SpellChecker("en_GB")
+filenameslist = glob.glob("../_posts/*.md") #loads files
 pwl = enchant.request_pwl_dict("Dict.txt")
 check1 = 0
-wordswrong = open("WordsWrong.txt", "w+")
-filecheck = open("FileCheck.txt", "w+")
-
+wordswrong = open("WordsWrong.txt", "w+") #creates new file to save the words that were spelt wrong
+filecheck = open("FileCheck.txt", "w+") #creates new file to save which file it checked
