143ef7b492864cecb53d9dd1c5a007d8c9337245
Devdeve <russell.seers@gmail.com>
2016-04-04 11:34:34 +0100

    Update Spellchecker.py
    Adding in notes to understand easier

--- _tests/Spellchecker.py
+++ _tests/Spellchecker.py
@@ -2,11 +2,10 @@ import glob, os, enchant, codecs, logging, json, os.path, shutil, re
 from Variab import *
 from Funct import *
 errortotalprev = 0
+Filechecker();
 if os.path.exists("Prevscore.json"):
     with open('Prevscore.json', 'r') as f:
-        errortotalprev = json.load(f)
-Filechecker();
+        errortotalprev = json.load(f) #loads json file with errortotalprev in
 linechecker(errortotalprev);
-filecheck.close()
+filecheck.close() #closes both files that were opened to save contents
 wordswrong.close()
-
