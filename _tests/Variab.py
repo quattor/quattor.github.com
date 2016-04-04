5d9fd1283d1739775f2070b21f08c0771f4265cc
Devdeve <russell.seers@gmail.com>
2016-04-04 12:05:13 +0100

    Update Variab.py

--- _tests/Variab.py
+++ _tests/Variab.py
@@ -2,11 +2,11 @@ import glob, os, enchant, codecs, logging, json, os.path, shutil, re
 from enchant.checker import SpellChecker
 from enchant.tokenize import EmailFilter, URLFilter
 #logger = logging.getLogger('spellcheck')
-pwl = enchant.request_pwl_dict("Dict.txt") #add words to the dictionary used to test for spelling errors
+pwl = enchant.request_pwl_dict("./_tests/Dict.txt") #add words to the dictionary used to test for spelling errors
 chkr = SpellChecker("en_GB", filters=[URLFilter, EmailFilter])
 #chkr1 = SpellChecker("en_GB")
 filenameslist = glob.glob("./_posts/*.md") #loads files
-pwl = enchant.request_pwl_dict("Dict.txt")
+pwl = enchant.request_pwl_dict("./tests/Dict.txt")
 check1 = 0
 wordswrong = open("WordsWrong.txt", "w+") #creates new file to save the words that were spelt wrong
 filecheck = open("FileCheck.txt", "w+") #creates new file to save which file it checked
