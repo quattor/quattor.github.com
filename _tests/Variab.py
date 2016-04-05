4fd78e285ef98ede6b0694b3b85f35f4f26994a6
Devdeve <russell.seers@gmail.com>
2016-04-05 12:39:10 +0100

    Update Variab.py

--- _tests/Variab.py
+++ _tests/Variab.py
@@ -11,9 +11,9 @@ from enchant.checker import SpellChecker
 from enchant.tokenize import EmailFilter, URLFilter
 DIRECTORY_TESTS = os.path.dirname(os.path.realpath(__file__))
 DIRECTORY_POSTS = os.path.join(DIRECTORY_TESTS, '../_posts/')
-JSONSCORE = os.path.join(DIRECTORY_TESTS, 'prevscore.json')
+JSONSCORE = os.path.join(DIRECTORY_TESTS, 'Prevscore.json')
 # logger = logging.getLogger('spellcheck')
-pwl = enchant.request_pwl_dict(os.path.join(DIRECTORY_TESTS,"Dict.txt"))
+pwl = enchant.request_pwl_dict(os.path.join(DIRECTORY_TESTS, 'Dict.txt'))
 # add words to the dictionary used to test for spelling errors
 spellcheck = SpellChecker("en_GB", filters=[URLFilter, EmailFilter])
 chkr12 = SpellChecker("en_GB", filters=[URLFilter, EmailFilter])
